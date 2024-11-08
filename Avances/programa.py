f4b47231d16b091a8fca70298514db136b482a93
John <john.sinclair@upb.edu.co>
2024-11-07 22:36:07 -0500

    Programa

--- Avances/programa.py
+++ Avances/programa.py
@@ -1,6 +1,6 @@
 import os
-
-
+import csv
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -49,7 +49,7 @@ def submenu_txt():
     print("a. Contar numero de palabras")
     print("b. Reemplazar una palabra por otra")
     print("c. Contar el numero de caracteres")
-    opcion = input("Seleccione una opción: ")
+    opcion = input("Seleccione una opcion: ")
 
     if opcion.lower() == 'a':
         contar_palabras()
@@ -99,6 +99,132 @@ def contar_caracteres():
     caracteres_sin_espacios = len(contenido.replace(" ", ""))
     print(f"Total de caracteres con espacios: {total_caracteres}")
     print(f"Total de caracteres sin espacios: {caracteres_sin_espacios}")
+
+
+
+
+def submenu_csv():
+    print("\nProcesar archivo CSV (.csv):")
+    print("a. Mostrar las 15 primeras filas")
+    print("b. Calcular estadisticas")
+    print("c. Graficar una columna completa")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion.lower() == 'a':
+        mostrar_primeras_filas()
+    elif opcion.lower() == 'b':
+        calcular_estadisticas()
+    elif opcion.lower() == 'c':
+        graficar_columna()
+    else:
+        print("Asegurate de ingresar una letra que este dentro de las opciones disponibles")
+
+
+
+def mostrar_primeras_filas():
+    nombre_archivo = input("Ingrese el nombre del archivo CSV: ")
+    archivo = open(nombre_archivo, 'r')
+    lector_csv = csv.reader(archivo)
+    for i, fila in enumerate(lector_csv):
+        print(fila)
+        if i == 14:
+            break
+    archivo.close()
+
+
+
+def calcular_estadisticas():
+    nombre_archivo = input("Ingrese el nombre del archivo CSV: ")
+    columna = input("Ingrese el nombre de la columna a analizar: ")
+    
+    archivo = open(nombre_archivo, 'r')
+    lector_csv = csv.reader(archivo)
+    encabezados = next(lector_csv)
+    
+    if columna not in encabezados:
+        print("La columna ingresada no existe en el archivo.")
+        archivo.close()  
+        return
+
+    indice_columna = encabezados.index(columna)
+    datos = []
+
+    for fila in lector_csv:
+        try:
+            dato = float(fila[indice_columna])
+            datos.append(dato)
+        except ValueError:
+            continue
+
+    archivo.close() 
+
+    if datos:
+        cantidad = len(datos)
+        promedio = sum(datos) / cantidad
+        mediana= calcular_mediana(datos)
+        maximo = max(datos)
+        minimo = min(datos)
+
+        print(f"Cantidad de datos: {cantidad}")
+        print(f"Promedio: {promedio}")
+        print(f"Mediana: {mediana}")
+        print(f"Maximo: {maximo}")
+        print(f"Minimo: {minimo}")
+    else:
+        print("No se encontraron datos numericos en la columna.")
+
+
+
+def calcular_mediana(datos):
+    datos_ordenados = sorted(datos)  
+    cantidad = len(datos)  
+
+    if cantidad % 2 == 1:  
+        
+        mediana = datos_ordenados[cantidad // 2]
+    else:   
+        medio_1 = datos_ordenados[cantidad // 2 - 1]
+        medio_2 = datos_ordenados[cantidad // 2]
+        mediana = (medio_1 + medio_2) / 2
+    
+    return mediana
+
+
+
+def graficar_columna():
+    nombre_archivo = input("Ingrese el nombre del archivo CSV: ")
+    columna = input("Ingrese el nombre de la columna a graficar: ")
+
+    archivo = open(nombre_archivo, 'r',)
+    lector_csv = csv.reader(archivo)
+    encabezados = next(lector_csv)
+    
+    if columna not in encabezados:
+        print("La columna ingresada no existe en el archivo.")
+        archivo.close()  
+        return
+
+    indice_columna = encabezados.index(columna)
+    datos = []
+
+    for fila in lector_csv:
+        try:
+            dato = float(fila[indice_columna])
+            datos.append(dato)
+        except ValueError:
+            continue
+
+    archivo.close() 
+
+    if datos:
+        plt.figure(figsize=(10,5))
+        plt.plot(datos, color='orange')
+        plt.title(f"Grafico de la columna {columna}")
+        plt.xlabel("Indices")
+        plt.ylabel(columna)
+        plt.show()
+    else:
+        print("No se encontraron datos numericos en la columna.")
 
 
 
